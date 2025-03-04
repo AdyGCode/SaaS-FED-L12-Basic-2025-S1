bdda821618d3539c99679589df4bdcae27d8b74d
Adrian Gould <adrian.gould@nmtafe.wa.edu.au>
2025-03-04 17:27:50 +0800

    wip: Many-to-many demo

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,16 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Country extends Model
 {
     /** @use HasFactory<\Database\Factories\CountryFactory> */
-    use HasFactory;
+    use HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'short_code'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'short_code'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+        ];
+    }
 
     public function sports()
     {
         return $this->belongsToMany(Sport::class)->withPivot('position');
     }
+
+
 }
--- app/Models/Sport.php
+++ app/Models/Sport.php
@@ -12,6 +12,37 @@ class Sport extends Model
 
     protected $fillable = ['name'];
 
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'short_code'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+        ];
+    }
+
     public function countries()
     {
         return $this->belongsToMany(Country::class)->withPivot('position');

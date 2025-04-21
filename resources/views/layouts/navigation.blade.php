843c01b82d8d8d11e747693a7ce2013eb3278382
Ady G Code <ady.g.coder@gmail.com>
2025-04-21 23:24:05 +0800

    wip: Static Pages
    - create basic about in static
    - fix routes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,8 +25,12 @@
                         </x-nav-link>
                     @endauth
 
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                        {{ __('Home') }}
+                    <x-nav-link :href="route('sports.index')" :active="request()->routeIs('sports.*')">
+                        {{ __('Sports') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                        {{ __('About') }}
                     </x-nav-link>
 
                 </div>
@@ -105,6 +109,10 @@
             <div class="pt-2 pb-3 space-y-1">
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                    {{ __('About') }}
                 </x-responsive-nav-link>
             </div>
         @else
